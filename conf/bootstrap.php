01a1c4a0ea2d38313f1b4d26a6e3225d1bae1603
Haimov Ainur <hakimov2009@icloud.com>
2020-02-20 09:10:40 +0000

    T300496: Реализовать генератор классов для работы с процедурами firebird
    git-svn-id: https://svn.ilb.ru/repos/fbproxy/trunk@2 e9c0ba74-3e38-429e-a1c6-4516ecd29d0a

---
+++ conf/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+set_include_path(
+    __DIR__ . "/../classes"
+    . PATH_SEPARATOR . get_include_path()
+    . PATH_SEPARATOR . "metalibphp-1"
+);
+
+require_once "phplib-1/__autoload.php";
